a8ee5a29053732e3c3c5b93aec4f84731968b714
miig369 <m.gochera@outlook.com>
2024-12-07 14:19:15 +0000

    header

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+
+import { useState, useEffect } from "react";
+import { SelectedPage } from "./shared/types";
+import Navbar from "./components/navbar";
+
+
+function App() {
+
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar  isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  )
+}
+
+export default App
